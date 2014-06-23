191fd69b3779669c825a2de54fc5762bcdb08099
b115620 <mats.byfuglien@nav.no>
2014-06-23 14:53:57 +0200

    . to _

--- config/config.js
+++ config/config.js
@@ -4,10 +4,10 @@ var path = require('path'),
 var config = {
     root: rootPath,
     port: process.env.PORT || 1337,
-    dbUrl: process.env['db.url'] || "heltfeilhost",
-    dbUser: process.env['db.username'] || "hvemsomhelst",
-    dbPassword: process.env['db.password'] || "<hemmelig>",
-    dbSchema: process.env['db.schema'] || "R4321-B-ALTINN"
+    dbUrl: process.env['db_url'] || "heltfeilhost",
+    dbUser: process.env['db_username'] || "hvemsomhelst",
+    dbPassword: process.env['db_password'] || "<hemmelig>",
+    dbSchema: process.env['db_schema'] || "R4321-B-ALTINN"
 }
 
 module.exports = config
