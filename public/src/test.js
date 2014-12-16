c67e9dea0f550374c92bb98f02d024941eeca72d
b115620 <mats.byfuglien@nav.no>
2014-12-16 19:34:44 +0100

    Parsing vera json and getting a functional html table

--- public/src/test.js
+++ public/src/test.js
@@ -1,51 +1,256 @@
 var _ = require('lodash');
 var http = require('http');
+require('natural-compare-lite')
+fs = require('fs')
 
-http.get("http://b27isvl001.preprod.local:9080/version", function(res) {
+//http.get("http://b27isvl001.preprod.local:9080/version", function(res) {
 
-    var columns = ['Application', 't1', 't2', 't3', 't4', 'q1', 'q2', 'q3', 'q4', 'p']
-
-    var data = [
-        ['Aareg', '1.2.3', '', '', '', '3.2.1', '4.5.6', '6.9', '', ''],
-        ['Bbreg', '', '', '1.2.3-alphpa01-bata4-SNAPSHOT', '', '3.2.1', '', '6.9', '', '4.5.6'],
-        ['Ccreg', '4', '', '', '5.0', '6.1', '7.1', '', '8.5', ''],
-        ['Ddreg', '12.4.5.6.7.0', '12.4.5.6.7.0', '12.4.5.6.7.0', '12.4.5.6.7.0', '12.4.5.6.7.0', '12.4.5.6.7.0', '12.4.5.6.7.0', '12.4.5.6.7.0', '12.4.5.6.7.0'],
-        ['Eereg', '9.1', '', '10', '', '11.4', '11.6', '', '56.3', '3000']
+    var rawdata = [
+        {
+            "application": "Aareg",
+            "environment": "T1",
+            "version": "1.2.3",
+            "deployer": "Mats"
+        },
+        {
+            "application": "Aareg",
+            "environment": "P",
+            "version": "10 000 000",
+            "deployer": "Mats"
+        },
+        {
+            "application": "Aareg",
+            "environment": "Q1",
+            "version": "12.3",
+            "deployer": "Per"
+        },
+        {
+            "application": "Aareg",
+            "environment": "Q3",
+            "version": "23.1",
+            "deployer": "Espen"
+        },
+        {
+            "application": "Aareg",
+            "environment": "Q2",
+            "version": "3.2.1",
+            "deployer": "Max Mekker"
+        },
+        {
+            "application": "Bbreg",
+            "environment": "P",
+            "version": "1.2.3",
+            "deployer": "Mats"
+        },
+        {
+            "application": "Bbreg",
+            "environment": "Q1",
+            "version": "12.3",
+            "deployer": "Per"
+        },
+        {
+            "application": "Bbreg",
+            "environment": "Q3",
+            "version": "23.1",
+            "deployer": "Espen"
+        },
+        {
+            "application": "Bbreg",
+            "environment": "T3",
+            "version": "3.2.1",
+            "deployer": "Max Mekker"
+        },
+        {
+            "application": "Ccreg",
+            "environment": "T1",
+            "version": "4",
+            "deployer": "Kjetnt"
+        },
+        {
+            "application": "Ccreg",
+            "environment": "T4",
+            "version": "1234",
+            "deployer": "Per Kjell"
+        },
+        {
+            "application": "Ccreg",
+            "environment": "Q1",
+            "version": "23.1",
+            "deployer": "Espen"
+        },
+        {
+            "application": "Ccreg",
+            "environment": "Q2",
+            "version": "3.2.1",
+            "deployer": "Max Mekker"
+        },
+        {
+            "application": "Ccreg",
+            "environment": "Q4",
+            "version": "1.2.3-alpha03-beta2-stable-SNAPHOT",
+            "deployer": "Bjarne Betjent"
+        },
+        {
+            "application": "Ddreg",
+            "environment": "T1",
+            "version": "1234",
+            "deployer": "Skurven"
+        },
+        {
+            "application": "Ddreg",
+            "environment": "T2",
+            "version": "3.2.1",
+            "deployer": "Max Mekker"
+        },
+        {
+            "application": "Ddreg",
+            "environment": "T3",
+            "version": "3.3.3",
+            "deployer": "Max Mekker"
+        },
+        {
+            "application": "Ddreg",
+            "environment": "T4",
+            "version": "666",
+            "deployer": "Max Mekker"
+        },
+        {
+            "application": "Ddreg",
+            "environment": "Q1",
+            "version": "33.2.2.2.",
+            "deployer": "Max Mekker"
+        },
+        {
+            "application": "Ddreg",
+            "environment": "Q2",
+            "version": "22.3.3.33",
+            "deployer": "Max Mekker"
+        },
+        {
+            "application": "Ddreg",
+            "environment": "Q3",
+            "version": "13.5.63.23",
+            "deployer": "Max Mekker"
+        },
+        {
+            "application": "Ddreg",
+            "environment": "Q4",
+            "version": "1.34.2",
+            "deployer": "Max Mekker"
+        },
+        {
+            "application": "Ddreg",
+            "environment": "P",
+            "version": "12.4",
+            "deployer": "Max Mekker"
+        },
+        {
+            "application": "Eereg",
+            "environment": "T4",
+            "version": "1000",
+            "deployer": "Max Mekker"
+        },
+        {
+            "application": "Eereg",
+            "environment": "Q1",
+            "version": "2000",
+            "deployer": "Max Mekker"
+        },
+        {
+            "application": "Eereg",
+            "environment": "P",
+            "version": "3000",
+            "deployer": "Max Mekker"
+        },
+        {
+            "application": "Eereg",
+            "environment": "T3",
+            "version": "500",
+            "deployer": "Max Mekker"
+        },
+        {
+            "application": "Eereg",
+            "environment": "Q2",
+            "version": "250",
+            "deployer": "Max Mekker"
+        },
+        {
+            "application": "Eereg",
+            "environment": "Q4",
+            "version": "125",
+            "deployer": "Max Mekker"
+        }
     ]
 
-    var body = '';
+    function doingStuff(versionJson) {
 
-    res.on('data', function(chunk) {
-        body += chunk;
+        environments = getDistinctEnvironments(versionJson);
+
+        var applicationInstancesGroupedByApplication = _.chain(versionJson).groupBy(function(element) {
+            return element['application']
+
+        }).value();
+
+        var applications = Object.keys(applicationInstancesGroupedByApplication);
+        var tableContents = [];
+        _.forEach(applications, function(element) {
+            tableContents.push(generateApplicationRow(environments, element, applicationInstancesGroupedByApplication[element]))
+        });
+
+        generateHtml(environments, tableContents);
+
+    }
+
+
+
+function getDistinctEnvironments(versionData) {
+    var environments = _.chain(versionData).pluck('environment').uniq(function(element) {
+        return element.toLowerCase();
+    }).sortBy(String).value();
+    environments.unshift('Application');
+
+    return environments;
+}
+
+function generateApplicationRow(columns, appName, applicationInstances) {
+    var rowData = Array.apply(null, Array(columns.length -1))
+    rowData.unshift(appName)
+
+    for(var i = 1; i < columns.length; i++) {
+        var filtered = _.filter(applicationInstances, function(appInstance) {
+            return appInstance.environment === columns[i]
+        });
+
+        if(filtered.length === 1 ) {
+            rowData[i] = _.first(filtered);
+        }
+    }
+    return rowData;
+}
+
+doingStuff(rawdata);
+
+function generateHtml(environments, tableContents) {
+    var html = "<html><body>";
+    html += "<table border='1'>";
+    html += "<thead><tr><th>" + environments.join("</th><th>") + "</th><tr></thead>";
+
+    _.forEach(tableContents, function(row) {
+        var rowArray = [row[0]]
+        for(var i = 1; i < row.length; i++ ) {
+            if(row[i]) {
+                rowArray.push(row[i].version)
+            }
+            else {
+                rowArray.push("na")
+            }
+
+        }
+        html += "<tr><td>" + rowArray.join("</td><td>") + "</td></tr>";
     });
 
-    res.on('end', function() {
-        var response = JSON.parse(body)
-        var environments = _.chain(response).pluck('environment').uniq(true, function(element) {
-            return element.toLowerCase();
-        }).reverse();
-        console.log(environments);
+    html += "</body></html>";
 
-        var gb = _.chain(response).groupBy(function(element) {
-            return element['environment'].toLowerCase()
-
-        }).value()//.sortBy(function(elem){
-        //    console.log(elem)
-        //    return elem['environment'].toLowerCase();
-        //}).reverse();
-
-        console.log("=============")
-
-        console.log(gb)
-
-        //_.forEach(gb, function(element) {
-        //    _.forEach(element, function(subelement) {
-        //        console.log("** " + subelement);
-        //    })
-        //})
-
-    });
-
-}).on('error', function(e) {
-    console.log("Got error: ", e);
-});
+    console.log(html)
+    fs.writeFile("vera.html", html)
+}
