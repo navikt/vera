feac7eff31d6d9e1e0cc4ad841de58781802df2e
Horvi, Johnny <Johnny.Horvi@nav.no>
2015-01-09 11:09:06 +0100

    fikset feil stier etter rydding i struktur

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,5 +1,5 @@
 module.exports = function(app) {
-    var versionApi = require('../app/controllers/versionApi');
+    var versionApi = require('../controllers/versionApi');
 
     app.post('/version', versionApi.registerDeployment());
     app.get('/version', versionApi.getVersion());
--- backend/controllers/versionApi.js
+++ backend/controllers/versionApi.js
@@ -1,4 +1,4 @@
-var config = require("../../config/config");
+var config = require("../config/config");
 var Event = require('../models/event');
 var _ = require('lodash');
 
