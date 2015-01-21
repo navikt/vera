1e83ede540d525075a55914ed13783f3b4caa02b
Horvi, Johnny <Johnny.Horvi@nav.no>
2015-01-21 08:00:48 +0100

    asd

--- Gulpfile.js
+++ Gulpfile.js
@@ -11,10 +11,11 @@ var del = require('del');
 
 var paths = {
     js: ['./frontend/src/js/**/*.jsx', './app.jsx'],
-    css: ['./frontend/src/css/*.css'],
+    css: ['./frontend/src/css/**/*.css'],
     buildDir: './frontend/build',
     jsBuild: './frontend/build/js',
-    cssBuild: './frontend/build/css'
+    cssBuild: './frontend/build/css',
+    fontsBuild: './frontend/build/fonts'
 }
 
 del.sync(paths.buildDir);
@@ -38,12 +39,25 @@ gulp.task('copy-css', function () {
     var copyCss = function () {
         gulp.src('./node_modules/bootstrap/dist/css/bootstrap.css')
             .pipe(gulp.dest(paths.cssBuild));
+        gulp.src(paths.css)
+            .pipe(gulp.dest(paths.cssBuild));
+        gulp.src('./node_modules/font-awesome/css/font-awesome.css')
+            .pipe(gulp.dest(paths.cssBuild));
         gulp.src('./node_modules/react-select/dist/default.css')
             .pipe(rename('react-select.css'))
             .pipe(gulp.dest(paths.cssBuild));
     };
 
     del(paths.cssBuild, copyCss);
+});
+
+gulp.task('copy-fonts', function () {
+    var copyFonts = function () {
+        gulp.src('./node_modules/font-awesome/fonts/**/*')
+            .pipe(gulp.dest(paths.fontsBuild));
+    };
+
+    del(paths.fontsBuild, copyFonts);
 });
 
 gulp.task('copy-indexhtml', function () {
@@ -56,7 +70,7 @@ gulp.task('watch', function () {
     gulp.watch(paths.css, ['copy-css']);
 });
 
-gulp.task('default', ['watch', 'compile-js', 'copy-css', 'copy-indexhtml']);
+gulp.task('default', ['watch', 'compile-js', 'copy-css', 'copy-fonts', 'copy-indexhtml']);
 
 
 
