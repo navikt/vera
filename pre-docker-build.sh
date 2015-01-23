7ad1b42f4f5b7957cf4a1b90bd2853a9223c0ca7
root <root@a34mmvl003.devillo.no>
2015-01-23 15:20:35 +0100

    fiksa feil i predocker

--- pre-docker-build.sh
+++ pre-docker-build.sh
@@ -8,7 +8,7 @@ mkdir -p $WORKDIR/nodejs
 curl http://utviklerportalen.adeo.no/software/nodejs/nodejs-0.10.33-with-deps.el7.x86_64.tar.gz | tar xzfv - -C $WORKDIR/nodejs
 curl http://utviklerportalen.adeo.no/software/mongodb/mongodb-linux-x86_64-2.6.6.tgz > $WORKDIR/mongodb.tar.gz
 
-cd $WORKDIR && cp ../package.json . && npm install --production && rm package.json
+cd $WORKDIR && cp ../package.json . && npm install --production && rm -f package.json && cd -
 
 npm install
 
