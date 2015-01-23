2891f2f4d190f169815a5ab178d5d282bbf4f744
Horvi, Johnny <Johnny.Horvi@nav.no>
2015-01-23 10:52:45 +0100

    rename firehose -> log gulp setup fix

--- gulpfile.js
+++ gulpfile.js
@@ -8,60 +8,46 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var size = require('gulp-size');
 var del = require('del');
+var fs = require('fs');
 
 var paths = {
     js: ['./frontend/src/js/**/*.jsx', './app.jsx'],
-    css: ['./frontend/src/css/**/*.css'],
+    css: './frontend/src/css/**/*.css',
     buildDir: './frontend/build',
     jsBuild: './frontend/build/js',
     cssBuild: './frontend/build/css',
-    fontsBuild: './frontend/build/fonts'
+    fontsBuild: './frontend/build/fonts',
+    dockerBuild: './docker'
 }
 
-del.sync(paths.buildDir);
+
+gulp.task('clean', function(cb){
+    return del(paths.buildDir, cb);
+});
 
 gulp.task('compile-js', function () {
-    var compileJavascript = function () {
-        browserify('./app.jsx')
-            .transform(reactify)
-            .bundle()
-            .pipe(source('vera.js'))
-            .pipe(buffer())
-            //.pipe(uglify())
-            .pipe(size())
-            .pipe(gulp.dest(paths.jsBuild));
-    };
-
-    del(paths.jsBuild, compileJavascript);
-})
+    return browserify('./app.jsx')
+        .transform(reactify)
+        .bundle()
+        .pipe(source('vera.js'))
+        .pipe(buffer())
+        //.pipe(uglify())
+        .pipe(size())
+        .pipe(gulp.dest(paths.jsBuild));
+});
 
 gulp.task('copy-css', function () {
-    var copyCss = function () {
-        gulp.src('./node_modules/bootstrap/dist/css/bootstrap.css')
-            .pipe(gulp.dest(paths.cssBuild));
-        gulp.src(paths.css)
-            .pipe(gulp.dest(paths.cssBuild));
-        gulp.src('./node_modules/font-awesome/css/font-awesome.css')
-            .pipe(gulp.dest(paths.cssBuild));
-        gulp.src('./node_modules/react-select/dist/default.css')
-            .pipe(rename('react-select.css'))
-            .pipe(gulp.dest(paths.cssBuild));
-    };
-
-    del(paths.cssBuild, copyCss);
+    return gulp.src(['./node_modules/bootstrap/dist/css/bootstrap.css', paths.css, './node_modules/font-awesome/css/font-awesome.css'])
+        .pipe(gulp.dest(paths.cssBuild));
 });
 
 gulp.task('copy-fonts', function () {
-    var copyFonts = function () {
-        gulp.src('./node_modules/font-awesome/fonts/**/*')
-            .pipe(gulp.dest(paths.fontsBuild));
-    };
-
-    del(paths.fontsBuild, copyFonts);
+    return gulp.src('./node_modules/font-awesome/fonts/**/*')
+        .pipe(gulp.dest(paths.fontsBuild));
 });
 
 gulp.task('copy-indexhtml', function () {
-    gulp.src('./frontend/src/index.html')
+    return gulp.src('./frontend/src/index.html')
         .pipe(gulp.dest(paths.buildDir));
 });
 
@@ -70,7 +56,30 @@ gulp.task('watch', function () {
     gulp.watch(paths.css, ['copy-css']);
 });
 
-gulp.task('default', ['watch', 'compile-js', 'copy-css', 'copy-fonts', 'copy-indexhtml']);
 
+gulp.task('docker-clean', function (cb) {
+    return del(paths.dockerBuild, cb);
+});
 
+gulp.task('copy-frontend', ['build', 'docker-clean'], function () {
+    return gulp.src(paths.buildDir + '/**/*')
+        .pipe(gulp.dest(paths.dockerBuild + '/frontend'));
+});
 
+gulp.task('copy-serverjs',['docker-clean'], function () {
+    return gulp.src('./server.js')
+        .pipe(gulp.dest(paths.dockerBuild));
+});
+
+gulp.task('copy-backend',['docker-clean'], function(){
+    return gulp.src('./backend/**/*')
+        .pipe(gulp.dest(paths.dockerBuild + '/backend'));
+});
+
+gulp.task('default', ['watch', 'build']);
+
+gulp.task('build',['clean'], function(){
+    gulp.start('compile-js', 'copy-css', 'copy-fonts', 'copy-indexhtml');
+});
+
+gulp.task('docker', ['docker-clean', 'build', 'copy-frontend', 'copy-backend', 'copy-serverjs']);
