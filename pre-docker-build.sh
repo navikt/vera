c2aed103a58ed776aef8c50dfd9428aaae652267
Horvi, Johnny <Johnny.Horvi@nav.no>
2015-02-09 17:10:07 +0100

    delte opp nedlasting og utpakking av nodejs i to steg da byggserveren ikke er helt frisk

--- pre-docker-build.sh
+++ pre-docker-build.sh
@@ -8,7 +8,9 @@ rm -rf $DOCKERDIR
 mkdir -p $DOCKERDIR/nodejs
 mkdir -p $DISTDIR
 
-curl http://utviklerportalen.adeo.no/software/nodejs/nodejs-0.10.33-with-deps.el7.x86_64.tar.gz | tar xzfv - -C $DOCKERDIR/nodejs
+#curl http://utviklerportalen.adeo.no/software/nodejs/nodejs-0.10.33-with-deps.el7.x86_64.tar.gz | tar xzfv - -C $DOCKERDIR/nodejs
+curl -O http://utviklerportalen.adeo.no/software/nodejs/nodejs-0.10.33-with-deps.el7.x86_64.tar.gz
+tar xzfv nodejs-0.10.33-with-deps.el7.x86_64.tar.gz -C $DOCKERDIR/nodejs
 
 cd $DISTDIR && cp ../../package.json . && npm install --production && rm -f package.json && cd -
 
