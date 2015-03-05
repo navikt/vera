2417925007ad67e2718fbd39c9617a99556d73ec
Horvi, Johnny <Johnny.Horvi@nav.no>
2015-03-05 13:05:50 +0100

    hostname -> address

--- backend/config/syslog.js
+++ backend/config/syslog.js
@@ -1,5 +1,5 @@
 var SysLogger = require('ain2');
 
-var logger = (process.env.NODE_ENV !== 'development') ? new SysLogger({tag: "vera", hostname: "dockerhost", port: 1514}) : console;
+var logger = (process.env.NODE_ENV !== 'development') ? new SysLogger({tag: "vera", address: "dockerhost", port: 1514}) : console;
 
 module.exports = logger;
