a0a073a1509d02a403470c1cb8e1d374a7acfdad
Horvi, Johnny <Johnny.Horvi@nav.no>
2015-03-05 08:15:17 +0100

    bruk dockerhost som hostnavn for syslog via udp og tag = vera

--- backend/config/syslog.js
+++ backend/config/syslog.js
@@ -1,5 +1,5 @@
 var SysLogger = require('ain2');
 
-var logger = (process.env.NODE_ENV !== 'development') ? new SysLogger() : console;
+var logger = (process.env.NODE_ENV !== 'development') ? new SysLogger({tag: "vera", hostname: "dockerhost"}) : console;
 
 module.exports = logger;
