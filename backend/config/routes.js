40de81a455bbb9227e3a0ce98c70bce5973a7775
b115620 <mats.byfuglien@nav.no>
2015-10-26 11:38:19 +0100

    deprecateded and removed old api function

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,9 +1,11 @@
 module.exports = function(app) {
     var versionApi = require('../controllers/versionApi');
+    var diffService = require('../controllers/diffService')
 
-    app.post('/version', versionApi.registerEvent()); //deprecated
-    app.get('/version', versionApi.getVersion()); //deprecated
-    app.post('/api/v1/deploylog', versionApi.registerEvent());
-    app.get('/api/v1/deploylog', versionApi.deployLog());
-    app.get('/api/v1/config', versionApi.config())
+    app.post('/version', versionApi.registerEvent); //deprecated
+    //app.get('/version', versionApi.getVersion); //deprecated
+    app.post('/api/v1/deploylog', versionApi.registerEvent);
+    app.get('/api/v1/deploylog', versionApi.deployLog);
+    app.get('/api/v1/config', versionApi.config);
+    app.get('/api/v1/diff', diffService.diffEnvironments)
 }
