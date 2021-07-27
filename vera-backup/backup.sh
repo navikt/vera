d370bd3976a9c50ac7ce8095bb86db38c7077768
frodesundby <frode.sundby@nav.no>
2021-07-27 12:19:52 +0200

    using correct bucket name in backup script

--- vera-backup/backup.sh
+++ vera-backup/backup.sh
@@ -6,7 +6,7 @@ if ! kubectl exec -it -n aura vera-mongo-0 -c mongod-container -- mongodump --ar
 fi
 
 backup_name="dump_$(date +"%Y-%m-%d_%H-%M")"
-if ! gsutil mv "./dump" "gs://vera-mongo-backup/$backup_name"; then
+if ! gsutil mv "./dump" "gs://vera-backup-bucket/$backup_name"; then
   echo "failed to upload backup to bucket"
   exit 1
 fi
