c7226276d1978cde37bc6dfc4b73dd0d79068c28
rbjornstad <roger.bjornstad@nav.no>
2021-08-09 09:46:15 +0200

    fix: using --gzip on mongodump

--- vera-backup/backup.sh
+++ vera-backup/backup.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-if ! kubectl exec -it -n aura vera-mongo-0 -c mongod-container -- mongodump --archive > ./dump; then
+if ! kubectl exec -it -n aura vera-mongo-0 -c mongod-container -- mongodump --archive --gzip > ./dump.gz; then
   echo "failed to execute mongodump"
   exit 1
 fi
 
 # compress dump before uploading to bucket
-tar zcf dump.tgz dump
-rm dump
 
 backup_name="dump_$(date +"%Y-%m-%d_%H-%M").tgz"
-if ! gsutil mv "./dump.tgz" "gs://vera-backup-bucket/$backup_name"; then
+if ! gsutil mv "./dump.gz" "gs://vera-backup-bucket/$backup_name"; then
   echo "failed to upload backup to bucket"
   exit 1
 fi
+
+rm dump.gz
+
