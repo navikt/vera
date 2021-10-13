9af31cf545f5193d83d2a55b2368b6243ef655fc
rbjornstad <roger.bjornstad@nav.no>
2021-10-13 14:50:28 +0200

    change port to 8080

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,7 +3,7 @@ var path = require('path'),
 
 var config = {
     root: rootPath,
-    port: process.env['PORT'] || 80,
+    port: process.env['PORT'] || 8080,
     dbUrl: process.env['VERADB_URL'] || "mongodb://127.0.0.1/deploy_log",
     dbUser: process.env['VERADB_USERNAME'] || "vera",
     dbPassword: process.env['VERADB_PASSWORD'] || "<hemmelig>",
