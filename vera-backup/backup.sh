0c205be5e13a868fdeda211362a9f8954eb9e9a6
Roger Bjørnstad <roger.bjornstad@nav.no>
2022-12-26 09:52:25 +0100

    use tmp

--- vera-backup/backup.sh
+++ vera-backup/backup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if ! kubectl exec -it -n aura vera-mongo-0 -c mongod-container -- mongodump --archive --gzip > ./dump.gz; then
+if ! kubectl exec -it -n aura vera-mongo-0 -c mongod-container -- mongodump --archive --gzip > /tmp/dump.gz; then
   echo "failed to execute mongodump"
   exit 1
 fi
@@ -8,7 +8,7 @@ fi
 # compress dump before uploading to bucket
 
 backup_name="dump_$(date +"%Y-%m-%d_%H-%M").gz"
-if ! gsutil mv "./dump.gz" "gs://vera-backup-bucket/$backup_name"; then
+if ! gsutil mv "/tmp/dump.gz" "gs://vera-backup-bucket/$backup_name"; then
   echo "failed to upload backup to bucket"
   exit 1
 fi
