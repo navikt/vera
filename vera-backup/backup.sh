cb7dc46f0954ba8eac7d99c256f9d1496f8fc0fe
Sten Røkke <sten.ivar.rokke@nav.no>
2024-01-05 09:16:47 +0100

    feat: new bucket for backups after moving to team-atom

--- vera-backup/backup.sh
+++ vera-backup/backup.sh
@@ -8,7 +8,7 @@ fi
 # compress dump before uploading to bucket
 
 backup_name="dump_$(date +"%Y-%m-%d_%H-%M").gz"
-if ! gsutil mv "/tmp/dump.gz" "gs://vera-backup-bucket/$backup_name"; then
+if ! gsutil mv "/tmp/dump.gz" "gs://vera-backup-bckt/$backup_name"; then
   echo "failed to upload backup to bucket"
   exit 1
 fi
